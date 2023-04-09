d1a8bb9c5d836f88cfaf2da7729736ab98e31ab2
vruivo <vruivo.code@gmail.com>
2023-04-09 21:06:06 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Simple project to experiment using a bluetooth (raw) serial console, with the HC-06 board.
+
+Adding a Bluetooth port on Windows 10:
+https://community.element14.com/technologies/internet-of-things/b/blog/posts/standard-serial-over-bluetooth-on-windows-10
+
+Listening on Linux with rfcomm:
+https://askubuntu.com/questions/248817/how-to-i-connect-a-raw-serial-terminal-to-a-bluetooth-connection
+
+There are also examples on the internet using Android and iOS apps.
+
+I created a bluetooth port on Windows + the Arduino IDE serial monitor.
+
+
+Based on code from the Pico-Examples https://github.com/raspberrypi/pico-examples/blob/master/uart/hello_uart/hello_uart.c
---
+++ hello_uart.c
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+
+/// \tag::hello_uart[]
+
+#define UART_ID uart0
+// #define BAUD_RATE 115200
+#define BAUD_RATE 9600
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+int main() {
+    // include "pico/binary_info.h"
+    // bi_decl(bi_program_description("This is a test binary."));
+    // bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+    // Set up our UART with the required speed.
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+    while (true) {
+        // Send out a character without any conversions
+        uart_putc_raw(UART_ID, 'A');
+
+        // Send out a character but do CR/LF conversions
+        uart_putc(UART_ID, 'B');
+
+        // Send out a string, with CR/LF conversions
+        uart_puts(UART_ID, " Hello, UART!\n");
+
+        sleep_ms(1500);
+    }
+    return 0;
+}
+
+/// \end::hello_uart[]
